38bdcf843b5ab582bc6d3796ff6a26955fa0602d
SametErdogan67 <samedi6774@gmail.com>
2023-08-28 23:00:08 +0300

    checkout

---
+++ project/recap-project/starter/src/pages/ProductList.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import CardTotal from "../components/CardTotal";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const ProductList = () => {
+  const url = process.env.REACT_APP_API_URL;
+  const [product, setProduct] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const { data } = await axios("B");
+      setProduct(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div className="container mt-3">
+      <div className={"bg-light d-sm-block d-md-flex"}>
+        <p className="text-center text-danger w-100">Loading....</p>
+
+        <>
+          <article id="product-panel" className="col-md-5">
+            <ProductCard />
+          </article>
+          <article className="col-md-5 m-3">
+            <CardTotal />
+          </article>
+        </>
+
+        <p className="text-center text-danger w-100">No products data...</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
